901b29e203cdb35ba7d1a8d2633c5cfe17f9f363
KenichiroWani <gundambaseball@i.softbank.jp>
2018-06-07 14:55:17 +0900

    delete m2rp

--- m2rp-master/m2rp-master/markdown-to-pug-rev/index.js
+++
@@ -1,9 +0,0 @@
-// var md = `<!-- extends /layout/base -->\n<!-- block abc -->\n\t![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")\n<!-- block def -->\n\t![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
-//
-// md2pug = new (require('./lib/markdown-to-pug'))();
-// let ret = md2pug.render(md);
-// console.log(md);
-// console.log();
-// console.log(ret);
-
-module.exports = require('./lib/markdown-to-pug');
